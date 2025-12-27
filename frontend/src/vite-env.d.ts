fb762b9f9753767f3cfec5c11f19e5f4bf074c0b
Leidy Nayely Vilca Choquemamani <leidyvilca3@gmail.com>
2025-12-26 22:47:51 -0500

    Update vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,3 +2,12 @@
 
 declare module "leaflet/dist/images/marker-icon.png";
 declare module "leaflet/dist/images/marker-shadow.png";
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
